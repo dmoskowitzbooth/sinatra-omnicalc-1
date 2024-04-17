c41566a64c8945a139fe83289d56e37f40138414
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-17 01:57:59 +0000

    3

--- app.rb
+++ app.rb
@@ -14,6 +14,30 @@ get("/square/new") do
 end
 
 get ("/square/results") do
-  
+  @the_num=params.fetch("users_number")
+  @the_result=params.fetch("users_number").to_f*params.fetch("users_number").to_f
   erb(:square_results)
+end
+
+get("/square_root/new") do
+
+  erb(:square_root)
+end
+
+get ("/square_root/results") do
+  @the_num=params.fetch("users_number")
+  @the_result=params.fetch("users_number").to_f**0.5
+  erb(:square_root_results)
+end
+
+get("/random/new") do
+
+  erb(:random)
+end
+
+get ("/random/results") do
+  @mini=params.fetch("users_min").to_f
+  @maxi=params.fetch("users_max").to_f
+  @results=rand(@mini..@maxi)
+  erb(:random_results)
 end
