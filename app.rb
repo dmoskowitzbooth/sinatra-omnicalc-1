aef3ce4d6ece9ac2fef5808b1f7e69127aa0f345
dmoskowitzbooth <165198611+dmoskowitzbooth@users.noreply.github.com>
2024-04-17 02:30:51 +0000

    4

--- app.rb
+++ app.rb
@@ -49,11 +49,9 @@ get("/payment/new") do
 end
 
 get ("/payment/results") do
-  @apr=params.fetch("users_apr").to_f.to_fs(:percentage, {:precision => 4})
+  @apr=params.fetch("users_apr").to_f
   @yrs=params.fetch("users_years").to_f
-  @prin=params.fetch("users_pv").to_f.to_fs(:currency)
+  @prin=params.fetch("users_pv").to_f
  
-  @results=
-
-  erb(:payment_results)
+  @results = @apr/100/12
 end
